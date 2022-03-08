632a3357a50dc27d78315242a89dab0196127594
KairosFaith <ian_ngoh@hotmail.com>
2022-03-08 15:25:00 +0800

    Update FaderUI.cs

--- FaderUI.cs
+++ FaderUI.cs
@@ -5,17 +5,18 @@ public class FaderUI : MonoBehaviour
 {
     public AudioMixer Mixer;
     public string ParamName;
-    public Slider slider;
+    Slider _slider;
     void Awake()
     {
-        slider.maxValue = 1;
-        slider.minValue = .0001f;
-        slider.onValueChanged.AddListener(SetVol);
+        _slider = GetComponent<Slider>();
+        _slider.maxValue = 1;
+        _slider.minValue = .0001f;
+        _slider.onValueChanged.AddListener(SetVol);
     }
     private void OnEnable()
     {
         Mixer.GetFloat(ParamName, out float faderValue);
-        slider.value = Mathf.Pow(10, faderValue / 20);
+        _slider.value = Mathf.Pow(10, faderValue / 20);
     }
     public void SetVol(float sliderValue)
     {
