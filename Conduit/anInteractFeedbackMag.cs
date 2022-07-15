4c3b2860127cc133927db2eea17bbeac641a7baa
KairosFaith <TA_TECH@mages.edu.sg>
2022-07-15 23:53:50 +0800

    clear out everything

--- Conduit/anInteractFeedbackMag.cs
+++ Conduit/anInteractFeedbackMag.cs
@@ -1,11 +1,6 @@
 using UnityEngine;
-using UnityEngine.Audio;
 [CreateAssetMenu(fileName = "UIMag", menuName = "AnopiaEngine/UI Feedback", order = 6)]
-public class anInteractFeedbackMag : IanAudioMag
+public class anInteractFeedbackMag : ScriptableObject
 {
     public ClipData Enter,Down,Interact,Up,Exit;
-    public override IanEvent LoadMag(anDriver driver, AudioMixerGroup output)
-    {
-        throw new System.NotImplementedException("Implement to anInteractable Directly");
-    }
 }
