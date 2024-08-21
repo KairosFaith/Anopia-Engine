3f2b2c5f6f71b265387522a368bb286e0aef1bfa
Ian Ngoh <ian@mages.edu.sg>
2024-08-21 11:27:31 +0800

    added ygnium synchro
    using only one source object

---
+++ Foundation/YgniumSynchro.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+//public delegate void OnBeatFunc(int barCount, int beatCount);
+public class YgniumSynchro : IanSynchro
+{
+    public double CurrentBeat;//current is past
+    public AudioSource Source;
+    Action _Ygnite, _Shadow;
+    public override void StartSynchro(double startTime)
+    {
+        StopSynchro();
+        SparkYgnium(1, 1, startTime);
+    }
+    public override void StopSynchro()
+    {
+        Source.Stop();
+        _Ygnite = null;
+        CurrentBeatCount = CurrentBarCount = 0;
+    }
+    void SparkYgnium(int beat, int bar, double timeCode)
+    {
+        Source.PlayScheduled(timeCode);
+        print(timeCode-AudioSettings.dspTime);//must be a positive value
+        _Ygnite = () => Ygnite(beat, bar, timeCode);
+        _Shadow = Shadow;
+    }
+    void Ygnite(int beat, int bar, double timeCode)
+    {
+        PlayOnBeat?.Invoke(beat, timeCode);
+        CurrentBeatCount = beat;
+        CurrentBarCount = bar;
+        CurrentBeat = timeCode;
+        _Shadow = Shadow;
+    }
+    void Shadow()
+    {
+        SparkYgnium(CurrentBeatCount + 1, CurrentBarCount + 1, CurrentBeat + Tempo.BeatLength);
+    }
+    void Update()
+    {
+        if (Source.time > 0)
+        {
+            _Ygnite?.Invoke();
+            _Ygnite = null;
+        }
+        else if(!Source.isPlaying)
+        {
+            _Shadow?.Invoke();
+            _Shadow = null;
+        }
+        CurrentDSPTime = AudioSettings.dspTime;
+        SourceTime = Source.time;
+        SourcePlaying = Source.isPlaying;
+    }
+    public double CurrentDSPTime, SourceTime;
+    public bool SourcePlaying;
+}
